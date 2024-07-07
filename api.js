301af1c8b5e04ab2daeb3d626fb070b1a5a3b26f
FAISAL <85350108+faisal-kursheedali@users.noreply.github.com>
2024-07-07 22:17:49 +0530

    Update api.js

--- api.js
+++ api.js
@@ -9,3 +9,19 @@ let serverURL = "https://insta-profile-link.vercel.app/api";
 fetch(`${serverURL}/portfolio/${USER_ID}/${dateTime}`)
   .then((res) => res.json())
   .then((val) => console.log(val));
+
+var username = prompt("what's your name?");
+username === null || username === "" ? (username = "null") : null;
+fetch(
+  `${serverURL}/portfolio_name_give/${username.toString()}/${USER_ID}/${dateTime}`
+)
+  .then((res) => res.json())
+  .then((val) => console.log(val));
+
+// processing
+var welcomemessage =
+  username +
+  ", you're special✨ & You deserve a star💫\n\nI appreciate your curiosity to know about me.\nThanks for caring.\nYou brighten my day and make me happy😀 \n\n You’re the best💛, thank you " +
+  username;
+//output
+alert(welcomemessage);
